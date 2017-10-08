c36a6ff94ae0857bcd6f57e91df6a70e8434a92a
JuHyeongLee <dlwngud24@naver.com>
2017-10-08 17:00:05 +0900

    utils.c

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,42 @@
+#include <stdio.h>
 #include <string.h>
-
+#include <unistd.h>
 #include "commands.h"
+#define MAX_PATH 260
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
+ 
+ if( chdir(argv[1])==0){
   return 0;
+  }else
+   return -1;
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-
+ 
+  char buffer[MAX_PATH] ={0};
+  char* cwd = NULL;
+  cwd = getcwd(buffer,MAX_PATH);
+  printf("%s\n",cwd);
   return 0;
+   
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+ if(argc ==2 && strcmp(argv[0],"cd")==0){
+ 
+ return 1;
+ }else
+  return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(argc ==1 && strcmp(argv[0],"pwd")==0){
   return 1;
+ }else
+ return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,36 @@
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
+
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+
+	*argv =(char**)malloc(sizeof(char*)*10);
+		
+	char cpy[100] ={0};
+	strcpy(cpy,command);	
+
+	char* tok;
+	tok = strtok(cpy," \n\t");
+	
+	int count =0;
+	
+	while( tok!=NULL){
+        (*argv)[count] = (char*)malloc(sizeof(char)*10);
+
+        strcpy((*argv)[count], tok);
+	tok = strtok(NULL," \n\t");       
+	count++;
+	 }
+	
+	*argc  = count;
+
+	if((*argc)==0)
+	{
+	(*argv)[0] = (char*)malloc(sizeof(char)*10);
+	strcpy((*argv)[0],"");
+	(*argc)=1;
+	}
 }
